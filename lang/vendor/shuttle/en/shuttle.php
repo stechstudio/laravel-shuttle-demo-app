6c07424cd700be8e0a0d305a3da370c380ec9e2e
Haz <me@hazjohnson.com>
2022-07-07 17:45:57 +0300

    wip

---
+++ lang/vendor/shuttle/en/shuttle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'drop_files'            => 'Drop files to upload',
+    'preparing'             => 'Preparing...',
+    'remaining'             => 'remaining',
+    'finished_successfully' => 'Finished successfully',
+    'finished_with_errors'  => 'Finished with errors',
+    'abort_upload'          => 'Are you sure you want to leave this page? Uploads in progress will be cancelled.',
+];
