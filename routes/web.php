7bf38ee299ea3988fdfde785573c4e91c4f0b6b9
HazJ <me@hazjohnson.com>
2022-09-29 18:11:53 +0300

    Tweaks

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 use App\Http\Livewire\FileBrowser;
 use App\Http\Livewire\Folder;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use STS\Shuttle\Facades\Shuttle;
-
-Auth::login(user: User::find(1));
 
 Route::redirect(uri: '/', destination: '/folders');
 Route::get(uri: '/folders', action: FileBrowser::class)->name('folders.index');
